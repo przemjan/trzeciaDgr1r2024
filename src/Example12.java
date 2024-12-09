cf1b07908e1b59162b56689fd55e3af63f50f04a
przemjan <przemjanisz2@gmail.com>
2024-12-09 11:31:37 +0100

    Commit nr 3 -- wartość bezwzględna

--- src/Example12.java
+++ src/Example12.java
@@ -1,2 +1,14 @@
 public class Example12 {
+    public static void main(String[] args) {
+        int number = -1;
+
+        number = (number < 0) ? -number :number;
+
+        if(number < 0)
+        {
+            number = -number;
+        }
+        Math.abs( number);
+        System.out.println(number);
+    }
 }
